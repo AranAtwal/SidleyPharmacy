40d364d28410775154776951840dd587a285aa3f
Aran <aranatwal96@gmail.com>
2023-10-31 20:02:21 +0000

    Added hero and image sizing css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,6 +15,31 @@ function Home() {
 							alt="pharamcy staff"
 						/>
 					</div>
+					<div className="hero-services">
+						<h1 className="hero-services-title">
+							NHS and Private Pharmacy Services
+						</h1>
+						<ul className="hero-serices-list">
+							<li>Order your NHS Perscriptions</li>
+							<li>
+								Avoid waiting for your GP, book NHS and private
+								appointments via the app today.
+							</li>
+							<li>Get your NHS prescriptions delivered.</li>
+							<li>Manage your loved ones Health care needs</li>
+						</ul>
+						<div className="nhs-login">
+							<a
+								href="https://www.nhs.uk/nhs-app/account/"
+								className="nhs-login-button"
+							>
+								<span className="nhs-login-logo">NHS</span>
+								<span className="nhs-login-text">
+									Continue with NHS Login
+								</span>
+							</a>
+						</div>
+					</div>
 				</section>
 			</main>
 		</div>
